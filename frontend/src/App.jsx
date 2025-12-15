1803a30f8f4b8c425f2cee8e1a030d5e59f08aa1
abhay <imabhaysachan@gmail.com>
2025-12-15 15:36:03 +0530

    add application route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import {Route,Routes} from "react-router-dom"
 import viteLogo from '/vite.svg'
 import './App.css'
 import LandingPage from './pages/Landing'
+import Application from './pages/Application'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <LandingPage/>
-    </>
+    return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/app" element={<Application />} />
+    </Routes>
   )
+  
 }
 
 export default App
