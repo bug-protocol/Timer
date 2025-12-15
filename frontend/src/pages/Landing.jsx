014c33bb31768fdc5e4f0d364a81120a852b7c2a
abhay <imabhaysachan@gmail.com>
2025-12-15 15:21:10 +0530

    add prelinimary landing page

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,7 @@
+function LandingPage() {
+    return <section className=" min-h-screen flex flex-col justify-center items-center">
+        <h1 className="text-3xl font-bold mb-2">Timer</h1>
+        <button className="border px-2 py-1 rounded bg-green-500 text-white">Start</button>
+        </section>
+}
+export default LandingPage;
